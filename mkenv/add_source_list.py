ecc3d480d6d2b71ac2026ced1c6784f384be5a0e
MrLYC <imyikong@gmail.com>
2014-11-18 21:34:49 -0800

    add add_source_list.py to modify sources.list

--- mkenv/add_source_list.py
+++ mkenv/add_source_list.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
 
+source_list = set(l.strip() for l in
+"""
 deb http://mirrors.163.com/ubuntu/ precise-updates main restricted
 deb-src http://mirrors.163.com/ubuntu/ precise-updates main restricted
 deb http://mirrors.163.com/ubuntu/ precise universe
@@ -11,3 +15,15 @@ deb http://mirrors.163.com/ubuntu/ precise-updates multiverse
 deb-src http://mirrors.163.com/ubuntu/ precise-updates multiverse
 deb http://mirrors.163.com/ubuntu/ precise-backports main restricted universe multiverse
 deb-src http://mirrors.163.com/ubuntu/ precise-backports main restricted universe multiverse
+""".split("\n"))
+
+source_list_path = "/etc/apt/sources.list"
+
+sys_source_list = set()
+
+with open(source_list_path, "a+") as fp:
+    sys_source_list.update(l.strip() for l in fp)
+
+    source_list = source_list - sys_source_list
+
+    fp.writelines("\n%s" % source for source in source_list)
