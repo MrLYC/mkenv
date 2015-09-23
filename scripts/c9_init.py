3abeacb5d89f61d73a90e59431fb3758e945d899
MrLYC <imyikong@gmail.com>
2015-09-23 03:12:48 +0000

    glone mkenv to /tmp/mkenv

--- scripts/c9_init.py
+++ scripts/c9_init.py
@@ -4,7 +4,8 @@
 import os
 
 
-os.system("git clone https://github.com/MrLYC/mkenv.git")
-os.system("cd mkenv/mkenv; make git-alias")
+os.system("git clone https://github.com/MrLYC/mkenv.git /tmp/mkenv")
+os.system("cd /tmp/mkenv/mkenv; make git-alias")
 os.system("git config --global user.name MrLYC")
 os.system("git config --global user.email imyikong@gmail.com")
+os.system("git config --global core.editor vim")
